d37315662131150234409420557d55d50d603fd4
unknown <hamid-savana@hotmail.fr>
2019-08-14 17:04:32 +0100

    tables creations finished

--- config/setup.php
+++ config/setup.php
@@ -11,30 +11,50 @@ try {
   echo "Database created successfully\n";
   } catch (PDOException $e) {
     echo "ERROR CREATING DB: \n".$e->getMessage()."\nAborting process\n";
-    // echo "Database re-created successfully\n";
-    // $sql = "DROP DATABASE `".$DB_NAME."`";
-    // $dbh->exec($sql);
-    // $sql = "CREATE DATABASE `".$DB_NAME."`";
-    // $dbh->exec($sql);
     // die();
   }
   // CREATE TABLES
-  try {
-    // Connect to DATABASE
-    $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE `users` (
-          `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-          `username` VARCHAR(50) COLLATE utf8_bin NOT NULL,
-          `email` VARCHAR(100) COLLATE utf8_bin NOT NULL,
-          `password` VARCHAR(255) COLLATE utf8_bin NOT NULL,
-          `token` VARCHAR(50) NOT NULL,
-          `verified` VARCHAR(1) NOT NULL DEFAULT 'N'
-        )";
-    $dbh->exec($sql);
-    echo "USERS table created !\n";
-    $sql = null;
-    } catch (PDOException $e) {
-      echo "ERROR CREATING TABLES\n";
-    }
+try {
+  // Connect to DATABASE
+  $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "CREATE TABLE `users` (
+        `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(50) NOT NULL,
+        `email` VARCHAR(100) NOT NULL,
+        `password` VARCHAR(255) NOT NULL,
+        -- `token` VARCHAR(50) NOT NULL,
+        -- `verified` VARCHAR(1) NOT NULL DEFAULT 'N'
+      )";
+  $dbh->exec($sql);
+  echo "USERS table created !\n";
+  $sql = null;
+  $sql = "CREATE TABLE `posts` (
+        `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(50) NOT NULL,
+        `picture` VARCHAR(255) NOT NULL,
+      )";
+  $dbh->exec($sql);
+  echo "PICTURES table created !\n";
+  $sql = null;
+  $sql = "CREATE TABLE `comments` (
+        `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(50) NOT NULL,
+        `picture_id` VARCHAR(255) NOT NULL,
+        `comment` VARCHAR(255) NOT NULL,
+      )";
+  $dbh->exec($sql);
+  echo "COMMENTS table created !\n";
+  $sql = null;
+  $sql = "CREATE TABLE `likes` (
+        `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(50) NOT NULL,
+        `picture_id` VARCHAR(255) NOT NULL,
+      )";
+  $dbh->exec($sql);
+  echo "COMMENTS table created !\n";
+  $sql = null;
+  } catch (PDOException $e) {
+    echo "ERROR CREATING TABLES :".$e->getMessage()."\n";
+  }
 ?>
