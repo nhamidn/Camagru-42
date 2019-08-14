fbe02e27b3d0a2685c99a96c84f966417ce1c16c
unknown <hamid-savana@hotmail.fr>
2019-08-14 13:57:23 +0100

    creating tables 2

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 $DB_NAME = "cam42";
 $DB_DSN = "mysql:host=127.0.0.1;dbname=".$DB_NAME;
-$DB_DSN_LIGHT = "mysql:host=127.0.0.1";
 $DB_USER = "root";
+$HOST = "127.0.0.1";
 $DB_PASSWORD = "tiger";
 ?>
--- config/setup.php
+++ config/setup.php
@@ -4,18 +4,37 @@ include 'database.php';
 // CREATE DATABASE
 try {
   // Connect to Mysql server
-  $dbh = new PDO($DB_DSN_LIGHT, $DB_USER, $DB_PASSWORD);
+  $dbh = new PDO("mysql:host=$HOST", $DB_USER, $DB_PASSWORD);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "CREATE DATABASE `".$DB_NAME."`";
   $dbh->exec($sql);
   echo "Database created successfully\n";
   } catch (PDOException $e) {
-    //echo "ERROR CREATING DB: \n".$e->getMessage()."\nAborting process\n";
-    echo "Database re-created successfully\n";
-    $sql = "DROP DATABASE `".$DB_NAME."`";
-    $dbh->exec($sql);
-    $sql = "CREATE DATABASE `".$DB_NAME."`";
-    $dbh->exec($sql);
-    exit(-1);
+    echo "ERROR CREATING DB: \n".$e->getMessage()."\nAborting process\n";
+    // echo "Database re-created successfully\n";
+    // $sql = "DROP DATABASE `".$DB_NAME."`";
+    // $dbh->exec($sql);
+    // $sql = "CREATE DATABASE `".$DB_NAME."`";
+    // $dbh->exec($sql);
+    // die();
   }
+  // CREATE TABLES
+  try {
+    // Connect to DATABASE
+    $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE `users` (
+          `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          `username` VARCHAR(50) COLLATE utf8_bin NOT NULL,
+          `email` VARCHAR(100) COLLATE utf8_bin NOT NULL,
+          `password` VARCHAR(255) COLLATE utf8_bin NOT NULL,
+          `token` VARCHAR(50) NOT NULL,
+          `verified` VARCHAR(1) NOT NULL DEFAULT 'N'
+        )";
+    $dbh->exec($sql);
+    echo "USERS table created !\n";
+    $sql = null;
+    } catch (PDOException $e) {
+      echo "ERROR CREATING TABLES\n";
+    }
 ?>
