155cdde7b188c3beef2e2be644f13b4ee59eae17
unknown <hamid-savana@hotmail.fr>
2019-08-16 02:31:09 +0100

    js

--- views/header.php
+++ views/header.php
@@ -1,14 +1,34 @@
 <?php
   session_start();
+  if (empty($_SESSION[username]))
+  {
 ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Camagru</title>
-  </head>
-  <body>
-
-  </body>
-</html>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="http://localhost/">Camagru</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation" onclick="toggle_btn()">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <script type="text/javascript">
+  function toggle_btn() {
+    var nav = document.getElementById("navbarNav");
+    if (nav.className === "collapse navbar-collapse") {
+      nav.className = "collapse.show navbar-collapse";
+    } else {
+      nav.className = "collapse navbar-collapse";
+    }
+  }
+  </script>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Register</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</header>
+<?php } ?>
