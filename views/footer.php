fc2e0ef16f1958a627eda7ea815acb640e9aa7d6
unknown <hamid-savana@hotmail.fr>
2019-08-17 22:22:44 +0100

    footer

---
+++ views/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="card" style="position: fixed;bottom: 0;width: 100%;margin-bottom: 0px;">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3 bg-light">© 2019 Copyright:
+    <a href="https://github.com/nhamidn"> nhamid</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
