c612691d112059cc93855f3816f835503e50add5
unknown <hamid-savana@hotmail.fr>
2019-08-21 19:25:50 +0100

    test code

--- test.php
+++ test.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html>
+<?php
+session_start();
+session_destroy();
+header ("Location index.php");
+?>
