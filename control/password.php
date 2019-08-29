d8f9def0b6a33d218ee5c2da0cf47c5c0eb79355
unknown <hamid-savana@hotmail.fr>
2019-08-29 17:11:32 +0100

    .

--- control/password.php
+++ control/password.php
@@ -10,9 +10,14 @@ if (empty($_SESSION[token])) {
 }
 if (!empty($_SESSION[token]) || !empty($_SESSION[username])) {
   if (empty($_POST["rpass2"]) || empty($_POST["rpass"])) {
-      // header("Location: ../password.php?token=" . $_SESSION[token] . "&error=Please enter a new password !");
+    // if (!empty($_SESSION[token])) {
+    //   header("Location: ../password.php?token=" . $_SESSION[token] . "&error=Please enter a new password !");
+    // } else {
+    //     header("Location: ../password.php?error=Please enter a new password !");
+    // }
       header("Location: ../index.php");
       exit();
   }
 }
+
 ?>
