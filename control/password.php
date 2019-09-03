8a2a3dc28b2b1b710f850a1b20c2131d391565a9
unknown <hamid-savana@hotmail.fr>
2019-09-03 22:56:49 +0100

    reset password script

--- control/password.php
+++ control/password.php
@@ -18,6 +18,35 @@ if (!empty($_SESSION[token]) || !empty($_SESSION[username])) {
       header("Location: ../index.php");
       exit();
   }
+  if (!empty($_SESSION[token]) && !empty($_SESSION[username])) {
+    try {
+      $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $query = $dbh->prepare("SELECT * FROM users WHERE username = :user");
+      $query->bindParam(':user', $_SESSION[username], PDO::PARAM_STR);
+      $query->execute();
+      $data = array();
+      $data = $query->fetch(PDO::FETCH_ASSOC);
+      if ($_SESSION[token] != $data['token']) {
+        header("Location: ../index.php");
+        exit();
+      } else {
+        // code...
+      }
+    } catch (PDOException $e) {
+      echo 'Error: '.$e->getMessage();
+      exit();
+    }
+  }
+  if ($_POST["rpass2"] != $_POST["rpass"]) {
+    if (!empty($_SESSION[token])) {
+      header("Location: ../password.php?token=" . $_SESSION[token] . "&error=Passwords does not match !");
+      exit();
+    } else {
+      header("Location: ../password.php?error=Passwords does not match !");
+      exit();
+    }
+  }
 }
 
 ?>
