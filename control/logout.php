3058ced1f9308a476f8f3dccdfc5c1c9e6b95b4f
unknown <hamid-savana@hotmail.fr>
2019-09-04 00:46:00 +0100

    csrf protection

--- control/logout.php
+++ control/logout.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
-session_destroy();
-header ("Location: /index.php");
+if ($_GET[token] != $_SESSION[logout]) {
+  header ("Location: /index.php");
+  exit();
+} else if (empty($_GET[token])) {
+  header ("Location: /index.php");
+  exit();
+} else {
+  $_SESSION[username] = "";
+  session_destroy();
+  header ("Location: /index.php");
+}
 ?>
