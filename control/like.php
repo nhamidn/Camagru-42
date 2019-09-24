df3ccb24b5859e59256f9950177789e652f49607
Noureddine Hamid <nhamid@e2r7p13.1337.ma>
2019-09-24 17:38:04 +0100

    mail notification done

--- control/like.php
+++ control/like.php
@@ -39,6 +39,39 @@
           $query->bindParam(':luname', $_SESSION[username], PDO::PARAM_STR);
           $query->bindParam(':lpicture', $_POST['lpicture'], PDO::PARAM_STR);
           $query->execute();
+          try {
+            $query1 = $dbh->prepare("SELECT * FROM posts WHERE picture = :thephoto");
+            $query1->bindParam(':thephoto', $_POST['lpicture'], PDO::PARAM_STR);
+            $query1->execute();
+            $datax = array();
+            $datax = $query1->fetch(PDO::FETCH_ASSOC);
+            try {
+              $query2 = $dbh->prepare("SELECT * FROM users WHERE username = :userpic");
+              $query2->bindParam(':userpic', $datax['username'], PDO::PARAM_STR);
+              $query2->execute();
+              $maildata = array();
+              $maildata = $query2->fetch(PDO::FETCH_ASSOC);
+
+              $who = $maildata['username'];
+              $whomail = $maildata['email'];
+
+              // Send mail notification
+              $subject = "User Notification";
+              $headers = 'From: <nhamid@student.1337.ma>';
+              $message = 'Hello ' . $maildata['username'] . ", " . $_SESSION[username] . ' has just liked one of your pictures.';
+              if ($maildata['notification'] == 'Y')
+                mail($whomail, $subject, $message, $headers);
+              //-----------------------
+
+
+            } catch (PDOException $e) {
+              echo 'Error: '.$e->getMessage();
+              exit();
+            }
+          } catch (PDOException $e) {
+            echo 'Error: '.$e->getMessage();
+            exit();
+          }
         } catch (PDOException $e) {
           echo 'Error: '.$e->getMessage();
           exit();
