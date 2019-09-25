fb44fc6f9d725f0ee79756df6c893d04af1c1bf4
Noureddine Hamid <nhamid@e2r7p13.1337.ma>
2019-09-25 23:27:48 +0100

    bug fix

--- index.php
+++ index.php
@@ -210,7 +210,6 @@
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if(this.readyState == 4 && this.status == 200) {
-				console.log(this.responseText);
 				if (this.responseText === "like") {
 					htmlContent = node.innerHTML.trim();
 					htmlContent++;
